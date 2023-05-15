797f68a8971a20a0e00ef43d348380617091c63d
School <bobicemj@gmail.com>
2023-05-15 22:58:06 +0200

    added Main class cleaned up code

--- Main.py
+++ Main.py
@@ -5,30 +5,13 @@ from datetime import time
 from autoclicker import Autoclicker
 from pyGUI import pyGUI
 
-
 if __name__ == '__main__':
-    # ac = Autoclicker()
-    # ac.start()
-    print(threading.active_count())
-
-    ac_thread = threading.Thread(target=Autoclicker().start())
+    print('lmao')
+    print('asshole',threading.active_count())
+    ac = Autoclicker()
+    ac_thread = threading.Thread(target=ac.start)
     ac_thread.start()
-    print(threading.active_count())
 
-    gui_thread = threading.Thread(target=pyGUI())
-    gui_thread.start()
-    print(threading.active_count())
-
-    # start the GUI event loop
-    # dpg.start_dearpygui()
-
-    # wait for the autoclicker thread to finish
-    # ac_thread.join()
-
-    # clean up the dearpygui context
-    # dpg.destroy_context()
-
-
-
-
-
+    GUI = pyGUI()
+    GUI_thread = threading.Thread(target=GUI.set_main_window)
+    GUI_thread.start()
--- pyGUI.py
+++ pyGUI.py
@@ -1,38 +1,29 @@
 import dearpygui.dearpygui as dpg
-import dearpygui.demo as demo
-from autoclicker import Autoclicker
-import threading
+# import dearpygui.demo as demo
+# import threading
 
-dpg.create_context()
-print('rewre')
-def callback(sender,data):
-    print(sender, "returned:", data)
+class pyGUI:
+    def __init__(self):
+        print('pyGUI created')
+        dpg.create_context()
 
-with dpg.window(label='jlrewj', no_title_bar=True, no_move=True, width=600, height=600):
-    # double click delay slider
-    dpg.add_button(label='flsdf')
-    slider = dpg.add_slider_float(min_value=0.03, max_value=1, callback=callback)
+    def callback(sender,data):
+        print(sender, "returned:", data)
 
-    # regular click delay slider
-    dpg.add_button(label='flsdf')
-    dpg.add_slider_float(min_value=0.03, max_value=1)
+    def set_main_window(self):
+        dpg.create_viewport(title='Custom Title', width=600, height=600)
+        with dpg.window(label='jlrewj', no_title_bar=True, no_move=True, width=600, height=600):
+            # double click delay slider
+            dpg.add_button(label='flsdf')
+            slider = dpg.add_slider_float(min_value=0.03, max_value=1, callback=self.callback)
 
-# dpg.destroy_context()
+            # regular click delay slider
+            dpg.add_button(label='flsdf')
+            dpg.add_slider_float(min_value=0.03, max_value=1)
 
-if __name__ == '__main__':
-    # ac = Autoclicker()
-    # ac.start()
-    print('asshole',threading.active_count())
-    ac = Autoclicker()
-    ac_thread = threading.Thread(target=ac.start)
-    ac_thread.start()
-
-    print('asshoe')
-
-    dpg.create_viewport(title='Custom Title', width=600, height=600)
-    dpg.setup_dearpygui()
-    dpg.show_viewport()
-    dpg.start_dearpygui()
+        dpg.setup_dearpygui()
+        dpg.show_viewport()
+        dpg.start_dearpygui()
 
     # clean up the dearpygui context
     # dpg.destroy_context()
